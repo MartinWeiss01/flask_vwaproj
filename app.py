a197d53d9415ede5ebd4406d5cf138f2d7f0f31d
xvyzhlov <xvyzhlov@mendelu.cz>
2022-12-06 17:10:08 +0100

    - create main project

--- app.py
+++ app.py
@@ -10,3 +10,10 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config.from_object('config')
 database.init_app(app)
+
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
