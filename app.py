afbfa5f7a51b8b4fab29528f0b95b4f3c324fa82
Martin Weiss <jsem@martinweiss.cz>
2022-12-22 15:20:34 +0100

    feat(Prices): add Current Prices page

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, flash, redirect, url_for, ses
 from views.auth import auth_bp
 from views.homepage import homepage
 from database import database
-import forms
+from service.materials_service import MaterialsService
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -11,6 +11,11 @@ database.init_app(app)
 
 app.register_blueprint(homepage, url_prefix='/')
 app.register_blueprint(auth_bp, url_prefix='/auth')
+
+@app.route('/prices')
+def view_current_prices():
+    prices = MaterialsService.get_current_prices()
+    return render_template("prices.html", prices=prices)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5001, debug=True)
