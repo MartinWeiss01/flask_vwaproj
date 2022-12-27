5538e2487ccea0e51d41abd9962b2c8b000dd54f
Martin Weiss <jsem@martinweiss.cz>
2022-12-27 16:07:19 +0100

    ci(Admin): add Materials Managing route

--- views/admin.py
+++ views/admin.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, render_template, session, redirect, url_for, request, flash
 
+from service.materials_service import MaterialsService
+
 admin_bp = Blueprint('admin', __name__)
 
-@admin_bp.route('/', methods=['GET', 'POST'])
-def preview():
-    return render_template('homepage/index.html')
+@admin_bp.route('/materials/manage', methods=['GET', 'POST'])
+def admin_update_prices():
+  prices = MaterialsService.get_current_prices()
+  return render_template('admin/materials/manage.html', prices=prices)
