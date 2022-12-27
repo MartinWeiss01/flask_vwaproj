68ef601a4185cf6bf7ed7344085c3b496afbfe2b
Martin Weiss <jsem@martinweiss.cz>
2022-12-27 16:31:38 +0100

    feat(Admin): add Insert New Material page

--- views/admin.py
+++ views/admin.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, session, redirect, url_for, request, flash
 
+import forms
 from service.materials_service import MaterialsService
 
 admin_bp = Blueprint('admin', __name__)
@@ -8,3 +9,14 @@ admin_bp = Blueprint('admin', __name__)
 def admin_update_prices():
   prices = MaterialsService.get_current_prices()
   return render_template('admin/materials/manage.html', prices=prices)
+
+@admin_bp.route('/materials/new', methods=['GET', 'POST'])
+def admin_new_material():
+  form = forms.NewMaterialForm(request.form)
+  if request.method == 'POST' and form.validate():
+      insertedId = MaterialsService.add_new_material(name=request.form['name'], price=request.form['price'])
+      if insertedId != -1:
+          return redirect(url_for('admin.admin_update_prices'))
+      else:
+          flash('Material with this name already exists')
+  return render_template('admin/materials/new.html', form=form)
