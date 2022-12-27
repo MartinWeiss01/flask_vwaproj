b06a604cd7e0c124b0a370c35b0a9c3c04d81e94
Martin Weiss <jsem@martinweiss.cz>
2022-12-27 16:34:34 +0100

    refactor(Admin): rename routing

--- views/admin.py
+++ views/admin.py
@@ -6,17 +6,17 @@ from service.materials_service import MaterialsService
 admin_bp = Blueprint('admin', __name__)
 
 @admin_bp.route('/materials/manage', methods=['GET', 'POST'])
-def admin_update_prices():
+def update_prices():
   prices = MaterialsService.get_current_prices()
   return render_template('admin/materials/manage.html', prices=prices)
 
 @admin_bp.route('/materials/new', methods=['GET', 'POST'])
-def admin_new_material():
+def new_material():
   form = forms.NewMaterialForm(request.form)
   if request.method == 'POST' and form.validate():
       insertedId = MaterialsService.add_new_material(name=request.form['name'], price=request.form['price'])
       if insertedId != -1:
-          return redirect(url_for('admin.admin_update_prices'))
+          return redirect(url_for('admin.update_prices'))
       else:
           flash('Material with this name already exists')
   return render_template('admin/materials/new.html', form=form)
