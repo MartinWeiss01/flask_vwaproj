959937b0b2cd6b4907b04ba2eff3e13fb16c9a99
xefanova <xefanova@mendelu.cz>
2023-01-09 00:25:49 +0100

    [Project] About us

--- app.py
+++ app.py
@@ -28,6 +28,10 @@ def view_all_stats():
     stats = MaterialsService.get_stats()
     return render_template("stats.html", stats=stats)
 
+@app.route('/about')
+def view_about_us():
+    return render_template("about_us.html")
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5001, debug=True)
 
